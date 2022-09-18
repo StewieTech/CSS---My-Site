419ea95bf4713ef3b0c28da61326fe715fbe353b
flystewie <77213085+flystewie@users.noreply.github.com>
2022-09-18 12:23:06 -0400

    Adding React & edabit folder

--- Session2/test.py
+++ Session2/test.py
@@ -1,30 +1,44 @@
-import unittest
+# import unittest
 
-class Employee:
-	def __init__(self, firstname, lastname):
-		self.firstname = firstname
-		self.lastname = lastname
-		self.fullname = firstname + ' ' + lastname
-		self.email = firstname.lower() + '.' + lastname.lower() + '@company.com'
+# class Employee:
+# 	def __init__(self, firstname, lastname):
+# 		self.firstname = firstname
+# 		self.lastname = lastname
+# 		self.fullname = firstname + ' ' + lastname
+# 		self.email = firstname.lower() + '.' + lastname.lower() + '@company.com'
         
 		
 
 
-emp_1 = Employee("Errol", "Stewart")
-print(emp_1.email)
+# emp_1 = Employee("Errol", "Stewart")
+# print(emp_1.email)
 		
-	#	emp_1 = Employee("John", "Smith")
-#emp_2 = Employee("Mary",  "Sue")
-#emp_3 = Employee("Antony", "Walker")
-#emp_1.fullname ➞ "John Smith"
+# 	#	emp_1 = Employee("John", "Smith")
+# #emp_2 = Employee("Mary",  "Sue")
+# #emp_3 = Employee("Antony", "Walker")
+# #emp_1.fullname ➞ "John Smith"
 
-#emp_2.email ➞ "mary.sue@company.com"
+# #emp_2.email ➞ "mary.sue@company.com"
 
-#emp_3.firstname ➞ "Antony"
-emp_1 = Employee("John", "Smith")
-emp_2 = Employee("Mary",  "Sue")
-emp_3 = Employee("Antony", "Walker")
-emp_4 = Employee("Joshua", "Senoron")
+# #emp_3.firstname ➞ "Antony"
+# emp_1 = Employee("John", "Smith")
+# emp_2 = Employee("Mary",  "Sue")
+# emp_3 = Employee("Antony", "Walker")
+# emp_4 = Employee("Joshua", "Senoron")
 
-# Test.assert_equals(emp_1.firstname, "John")
-# Test.assert_equals(emp_1.lastname, "Smith")
+# # Test.assert_equals(emp_1.firstname, "John")
+# # Test.assert_equals(emp_1.lastname, "Smith")
+
+class ones_threes_nines:
+	
+	def __init__ (self, num):
+		
+		self.nines = int(num / 9)
+
+        self.
+
+    
+		
+n1 = ones_threes_nines(82)
+
+print(n1.nines)
