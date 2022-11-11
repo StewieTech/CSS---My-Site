a6a62d334fc8556e090453564abe29551712dba3
flystewie <77213085+flystewie@users.noreply.github.com>
2022-11-11 10:17:59 -0500

    Complete LLabs

--- Session2/edabit/practice.js
+++ Session2/edabit/practice.js
@@ -6,8 +6,72 @@
 
 // const getFirstValue = arr => arr[0];
 
-function makePair(num1, num2) {
-    return [num1, num2]
+// function makePair(num1, num2) {
+//     return [num1, num2]
+// }
+
+// console.log(makePair(2,3))
+
+// function makePair(num1, num2) {
+// 	return [...arguments]
+// }
+
+// const makePair = (a,b) => [a,b];
+
+
+// const drop = (arr, val = 1) => {
+// 	return arr.slice(val);
+// }
+
+// const drop = (arr, val = 1) => arr.slice(val);
+
+// function printArray(number) {
+//     let newArray = [];
+  
+//     for(let i = 1; i <= number; i++) {
+//       newArray.push(i);
+//     }
+  
+//     return newArray;
+//   }
+
+//   print
+
+// let printArray = n => [...Array(n)].map((a,b)=> 1 + b * 1);
+
+// const printArray = num => [...Array(num + 1).keys()].slice(1);
+
+// const futurePeople = (p, n) => p + 360 * n
+
+const addTwo = (number) => {
+    number +=2;
+    return number;
 }
 
-console.log(makePair(2,3))
+console.log(addTwo(2));
+
+// const addTwo = () => {
+//     let number = 4
+// }
+
+// console.log(number)
+
+
+// 
+
+
+
+
+
+// let words = ['Hello', 'World', 'Cruel', 4, 5]
+
+// // for (let i = 0; i <words.length; i++) {
+// //     console.log(words[i]);
+// // }
+
+// for (let i = 4; i> -1; i--) {
+//     console.log(words[i]);
+// }
+
+
+
