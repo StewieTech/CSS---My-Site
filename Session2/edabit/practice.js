8c01e87650a1ae58853e43da9fb2a3e01678c508
flystewie <77213085+flystewie@users.noreply.github.com>
2022-11-11 09:18:38 -0500

    Start Javascript

---
+++ Session2/edabit/practice.js
@@ -0,0 +1,13 @@
+// console.log('hello world')
+
+// function getFirstValue(arr) {
+// 	return arr[0];
+// }
+
+// const getFirstValue = arr => arr[0];
+
+function makePair(num1, num2) {
+    return [num1, num2]
+}
+
+console.log(makePair(2,3))
