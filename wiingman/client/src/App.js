426d24255fd77824a9f002a626c5dff6e655fff9
flystewie <77213085+flystewie@users.noreply.github.com>
2023-07-22 14:50:59 -0400

    Update Lola with style

--- wiingman/client/src/App.js
+++ wiingman/client/src/App.js
@@ -2,7 +2,11 @@
 import logo from './logo.svg';
 import React, { useState} from 'react';
 import './App.css';
-import personImage from './lola2.webp';
+import { Container } from 'react-bootstrap';
+import {Row, Col, Form, Button } from 'react-bootstrap';
+
+console.log(process.env.REACT_APP_API_URL)
+console.log("Hey")
 
 // const pictureList = [ 'lola2.jpeg', 'lola.webp', 'lolac.png', 'lola3.png', 'lola4.png', 'lola5.png' ];
  const pictureList = [ 'lola2.png', 'lola.webp', 'lolac.png', 'lola3.png', 'lola4.png',  'lola6v4.png', 'lola5.png']
@@ -12,10 +16,12 @@ function App() {
   const [response, setResponse] = useState('');
   const [pictureIndex, setPictureIndex] = useState(0);
 
+  const Work = process.env.REACT_APP_API_URL ;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setPictureIndex((prevIndex) => (prevIndex + 1) % pictureList.length);
-    fetch(`https://lola-back.onrender.com/`, {
+    fetch(Work, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -37,63 +43,74 @@ function App() {
   });
 };
 
-var smiley = ';)'
+// var smiley = ';)'
 
-  return (
-    <div className="App">
-      <h1>Ask Your Wingman Lola A Question {smiley} </h1>
-      <header className="App-header">
-      <form onSubmit = {handleSubmit}>
-      
-      <div className="container">
-        <textarea
-        value = {message}
-        placeholder="Ask Lola anything ;)"
-        onChange = {(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button type ="submit">Submit</button>
-        </div>
-      </form>
-      <div>{response}</div>
-      {/* <div>Hey</div> */}
-      
+return (
 
 
-      <img
-          src={pictureList[pictureIndex]}
-          alt="Person"
-          className="person-image"
-        />
-        {/* <img src="Lola.webp" alt="Person" className="person-image" /> */}
+<Container fluid>
+    <h1 className="text-center mt-3">Ask Your Wingwoman Lola A Question 😉</h1>
+    <Row className="justify-content-center">
+      <Col xs={12} sm={8} md={6} lg={4}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formTextarea">
+            <Form.Control
+              as="textarea"
+              value={message}
+              placeholder="Ask Lola anything ;)"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" block>
+            Submit
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+    <Row className="justify-content-center mt-3">
+      <Col xs={12} sm={8} md={6} lg={4}>
+        <div><h2>{response}</h2></div>
+      </Col>
+    </Row>
+    <Row className="justify-content-center mt-3">
+      <Col xs={12} sm={8} md={6} lg={4}>
+        <img src={pictureList[pictureIndex]} alt="Person" className="person-image img-fluid" />
+      </Col>
+    </Row>
 
-        
-
+    <Row className="justify-content-center mt-3">
+      <Col xs={12} sm={8} md={6} lg={4}>
         <img src={logo} className="App-logo" alt="logo" />
+      </Col>
+    </Row>
 
 
+    <Row className="justify-content-center mt-3">
+      <Col xs={12} sm={8} md={6} lg={4}  className="text-center">
         <p>
-           <code>
-           Welcome to Wiingman :D
-           </code> 
+          <code>Welcome to Lola: Your Personal AI Wingwoman :D</code>
         </p>
-        <a
-          className="App-link"
-          href="https://apps.apple.com/app/apple-store/id1663430725"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am copying this app
-        </a>
-      
-       
-      </header>
+        <p className="text-center">
+          <a
+            className="App-link"
+            href="https://apps.apple.com/app/apple-store/id1663430725"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            I am copying this app
+          </a>
+        </p>
+      </Col>
+    </Row>
+  </Container>
 
-
-
-    </div>
-  );
+);
 }
+
 
 export default App
 
 // to runnnn app use : npm run dev 
+// aws s3 sync build/ s3://lola-s3
+// REACT_APP_API_URL=https://lola-back.onrender.com/
+// REACT_APP_API_URL=http://3.80.220.82:3001/
