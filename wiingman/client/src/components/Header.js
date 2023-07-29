417380d53f73ebc589cf28736a5e7c60040d473b
flystewie <77213085+flystewie@users.noreply.github.com>
2023-07-29 13:51:13 -0400

    Add compnents to App.js

---
+++ wiingman/client/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Navbar, Nav, Container} from 'react-native';
+
+const Header = () => {
+    return (
+
+        <div className="glass-container">
+    <h1 className="text-center mt-3">Ask Lola 😉</h1>
+    </div>
+        );
+};
+
+{/* <Navbar bg="light" expand="lg" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">Ask Lola 😉</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}; */}
+
+export default Header;
